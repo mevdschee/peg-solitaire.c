20a39a4f493826a75c5191c98e5824039c81c876
Maurits van der Schee <maurits@vdschee.nl>
2015-11-23 19:46:06 +0100

    Update peg-solitaire.c
    replaced \e with \033 for compatibility

--- peg-solitaire.c
+++ peg-solitaire.c
@@ -35,7 +35,7 @@ void drawBoard(board_s *board) {
 	int8_t x,y,count=0;
 
 	// move cursor to home position
-	printf("\e[H");
+	printf("\033[H");
 
 	for (y=0;y<(*board).size;y++) {
 		for (x=0;x<(*board).size;x++) {
@@ -52,9 +52,9 @@ void drawBoard(board_s *board) {
 		for (x=0;x<(*board).size;x++) {
 			if ((*board).cursor.x == x && (*board).cursor.y == y) {
 				if ((*board).selected) {
-					printf("\b|\e[7m%c\e[27m|",(*board).field[x][y]);
+					printf("\b|\033[7m%c\033[27m|",(*board).field[x][y]);
 				} else {
-					printf("\e[7m%c\e[27m ",(*board).field[x][y]);
+					printf("\033[7m%c\033[27m ",(*board).field[x][y]);
 				}
 			} else {
 				printf("%c ",(*board).field[x][y]);
@@ -67,7 +67,7 @@ void drawBoard(board_s *board) {
 	}
 	printf("                            \n");
 	printf("   arrow keys, q or enter   \n");
-	printf("\e[A"); // one line up
+	printf("\033[A"); // one line up
 }
 
 void rotateBoard(board_s *board) {
@@ -298,7 +298,7 @@ void setBufferedInput(bool enable) {
 void signal_callback_handler(int signum) {
 	printf("         TERMINATED         \n");
 	setBufferedInput(true);
-	printf("\e[?25h\e[0m");
+	printf("\033[?25h\033[0m");
 	exit(signum);
 }
 
@@ -323,7 +323,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	// reset, hide cursor and clear screen
-	printf("\e[0m\e[?25l\e[2J");
+	printf("\033[0m\033[?25l\033[2J");
 
 	// register signal handler for when ctrl-c is pressed
 	signal(SIGINT, signal_callback_handler);
@@ -402,7 +402,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	setBufferedInput(true);
-	printf("\e[?25h\e[0m");
+	printf("\033[?25h\033[0m");
 
 	return EXIT_SUCCESS;
 }
